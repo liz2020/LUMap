e44dc29a2c45d0b952c7d24a7f90122b4f0e2c63
liz2020 <lihaozhu2020@gmail.com>
2019-04-19 00:46:43 -0400

    remove the hardcode Marker, and get data through json request from backend

--- android/app/src/main/java/liz220/cse216/lehigh/edu/lehighmap/MapsActivity.java
+++ android/app/src/main/java/liz220/cse216/lehigh/edu/lehighmap/MapsActivity.java
@@ -1,14 +1,21 @@
 package liz220.cse216.lehigh.edu.lehighmap;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -16,6 +23,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ArrayList<Pinpoint> mMarkers = new ArrayList<Pinpoint>();
+    //TODO: replace ip with your ip to test on local. This will laster be replaced by heroku url once the backend in online
+    private final String backend_url = "http://IP:8888";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +35,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
+    
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -41,21 +48,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        populateMarkers(mMarkers);
-
-        // plot markers to map
-        for(int i =0; i< mMarkers.size();i++) {
-            mMap.addMarker(mMarkers.get(i).getMarker());
-        }
+        populateMarkers(mMap);
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.606691, -75.377016),16));
     }
 
-    private void populateMarkers(ArrayList<Pinpoint> mMarkers){
+    private void populateMarkers(final GoogleMap mMap){
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET,backend_url,null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("liz", response.toString());
+                        try {
+                            JSONArray json = response.getJSONArray("data");
+                            for (int i = 0; i < json.length(); ++i) {
+                                double lat = json.getJSONObject(i).getDouble("lat");
+                                double lng = json.getJSONObject(i).getDouble("lng");
+                                String title = json.getJSONObject(i).getString("title");
+                                Log.d("liz",title);
+                                mMarkers.add(new Pinpoint(lat, lng, title));
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                            Log.e("liz","populate marker fail");
+                        }
+                        // plot markers to map
+                        for(int i =0; i< mMarkers.size();i++) {
+                            mMap.addMarker(mMarkers.get(i).getMarker());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("liz", "Connection refused");
+                error.printStackTrace();
+            }
+        });
 
-        // later change this to http request
-        mMarkers.add(new Pinpoint(40.608962, -75.377874,"E.W. Fairchild-Martindale Library"));
-        mMarkers.add(new Pinpoint(40.609028, -75.377249,"Sinclair Laboratory"));
-        mMarkers.add(new Pinpoint(40.607257, -75.374035,"Taylor Gymnasium"));
+        // Add the request to the RequestQueue.
+        VolleySingleton.getInstance(this).addToRequestQueue(jsonRequest);
     }
 }
